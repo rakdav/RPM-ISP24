5f8f083d8ad66722023a8bcb33ffff5c293c2de8
Andrews <rakdav@inbox.ru>
2025-01-20 13:29:38 +0200

    lab4

---
+++ Lab3/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Circle : IFigure
+    {
+        public string Name { get; set;}
+        public double Radius { get; set; }
+        public double Area() => Math.PI * Radius * Radius;
+        public double Perimetr() => 2 * Math.PI * Radius;
+        public override string? ToString()
+        {
+            return $"{Name} площадью {Area():F2}, периметром {Perimetr():F2}";
+        }
+    }
+}
---
+++ Lab3/IFigure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal interface IFigure
+    {
+        string Name { get; set; }
+        double Area();
+        double Perimetr();
+    }
+}
