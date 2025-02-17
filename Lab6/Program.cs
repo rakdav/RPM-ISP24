a55469a3af1178edc6df1161ce6b7323d882d277
rakdav <rakdav@inbox.ru>
2025-02-17 17:14:30 +0200

    lab7

---
+++ Lab6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число n:");
+int n = int.Parse(Console.ReadLine()!);
+List<double> list = new List<double>();
+Random random = new Random();
+for (int i = 0; i < n; i++)
+{
+    list.Add(random.NextDouble() * 50-25);
+    Console.Write($"{list[i]:F2} ");
+}
+Console.WriteLine();
+Console.WriteLine(list.FindAll(p=>p>-15&&p<6).Count);
