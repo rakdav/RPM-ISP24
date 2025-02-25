ec750a2b6aa0fcf811084b1bbef39b3b2048b4aa
rakdav <rakdav@inbox.ru>
2025-02-25 19:15:17 +0200

    linq1

---
+++ Network1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+IPAddress localIp = new IPAddress(new byte[] { 127,0,0,1 });
+Console.WriteLine(localIp);
+IPAddress someIp = new IPAddress(0x0100007F);
+Console.WriteLine(someIp);
+
+IPAddress ip = IPAddress.Parse("192.168.113.1");
+IPAddress anyIp = IPAddress.Any;
+Console.WriteLine(anyIp);
+IPAddress localIP = IPAddress.Loopback;
+Console.WriteLine(localIP);
+IPAddress broadcastIP = IPAddress.Broadcast;
+Console.WriteLine(broadcastIP);
