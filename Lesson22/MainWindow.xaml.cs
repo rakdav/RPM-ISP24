d7b21bc860d778f3e4392a1582b6e42193809048
rakdav <rakdav@inbox.ru>
2025-03-17 18:08:05 +0200

    les22

--- Lesson22/MainWindow.xaml.cs
+++ Lesson22/MainWindow.xaml.cs
@@ -25,4 +25,20 @@ public partial class MainWindow : Window
         //string text = textBoxMessage.Text;
         //labelMessage.Content = text;
     }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        string op = (sender as Button)!.Content.ToString()!;
+        double var1 = double.Parse(first.Text);
+        double var2 = double.Parse(second.Text);
+        double result = 0;
+        switch (op)
+        {
+            case "+": result = var1 + var2;break;
+            case "-": result = var1 - var2;break;
+            case "*": result = var1 * var2;break;
+            case "/": result = var1 / var2;break;
+        }
+        Result.Content = $"{var1}{op}{var2}={result:F2}";
+    }
 }
