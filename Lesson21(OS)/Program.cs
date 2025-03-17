5d210e3d3c8a64ec7cdb01e798a001f1265ea766
rakdav <rakdav@inbox.ru>
2025-03-17 17:42:09 +0200

    les22

---
+++ Lesson21(OS)/Program.cs
@@ -0,0 +1,28 @@
+﻿Task task1 = new Task(()=>
+{
+    Console.WriteLine("Hello Task1!!");
+    Thread.Sleep(1000);
+});
+task1.Start();
+Task task2 = Task.Factory.StartNew(()=> 
+{
+    Console.WriteLine("Hello Task2!!");
+    Thread.Sleep(1000);
+});
+Task task3 = Task.Run(() =>
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("Hello Task3!!");
+});
+task1.Wait();
+task2.Wait();
+task3.Wait();
+Console.WriteLine(task1.Id);
+Console.WriteLine(task1.AsyncState);
+Console.WriteLine(task1.Status);
+Console.WriteLine(task1.Exception);
+Console.WriteLine(task1.IsCompleted);
+Console.WriteLine(task1.IsCanceled);
+Console.WriteLine(task1.IsFaulted);
+Console.WriteLine(task1.IsCompletedSuccessfully);
+
