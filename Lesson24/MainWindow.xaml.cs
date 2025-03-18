572dd7bf0c7817f5f697df0be8539d8912377461
rakdav <rakdav@inbox.ru>
2025-03-18 18:07:10 +0200

    18.03.2024

---
+++ Lesson24/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Lesson24.Model;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson24;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private Person person;
+    public MainWindow()
+    {
+        InitializeComponent();
+        person = new Person();
+        DataContext = person;
+    }
+}
---
+++ Lesson24/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson24.Model
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+        public string? SurName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
