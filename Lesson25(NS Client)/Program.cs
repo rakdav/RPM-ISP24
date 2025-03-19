11a93fb9b9e27ccfd9014db35585754cc2b0cc12
rakdav <rakdav@inbox.ru>
2025-03-19 16:42:42 +0200

    les25

---
+++ Lesson25(NS Client)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+    ProtocolType.Tcp);
+try
+{
+    await socket.ConnectAsync("127.0.0.1", 8888);
+    Console.WriteLine($"Подключение к {socket.RemoteEndPoint} установлено.");
+}
+catch(SocketException)
+{
+    Console.WriteLine($" Не удалось установить соединение с{socket.RemoteEndPoint}");
+}
