3706a6f00406b34d212c8ac62dc26191191520ab
rakdav <rakdav@inbox.ru>
2025-03-24 16:34:07 +0200

    valid

--- Lesson24/Model/Person.cs
+++ Lesson24/Model/Person.cs
@@ -1,16 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Lesson24.Model
 {
-    public class Person
+    public class Person:IDataErrorInfo
     {
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = String.Empty;
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            if (Name == null) error = "Поле пустое";
+                            if (Name!.All(c => char.IsLetter(c) || c == ' '))
+                                error = "Должны быть только буквы";
+                        }
+                        break;
+                    case "SurName":
+                        {
+                            if (SurName == null) error = "Поле пустое";
+                            if (SurName!.All(c => char.IsLetter(c) || c == ' '))
+                                error = "Должны быть только буквы";
+                        }
+                        break;
+                    case "Email":
+                        {
+                            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+                            if (!Regex.IsMatch(Email!, pattern, RegexOptions.IgnoreCase))
+                                error = "Неверный email";
+                        }
+                        break;
+
+                }
+                return error;
+            }
+        }
+
         public string? Name { get; set; }
         public string? SurName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
+
+        public string Error => throw new NotImplementedException();
     }
 }
