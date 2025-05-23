d4ceb90b6e7d01df37300dde943860c934fc36b2
rakdav <rakdav@inbox.ru>
2025-05-23 11:26:48 +0200

    Lab13

---
+++ Lab12-13/Converters/StringToZhanr.cs
@@ -0,0 +1,36 @@
+﻿using Lab12_13.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Lab12_13.Converters
+{
+    class StringToZhanr : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Zhanr zhanr=Zhanr.Children;
+            if (value == null) value = "Для детей";
+            switch (value.ToString())
+            {
+                case "Для детей": zhanr = Zhanr.Children; break;
+                case "Для взрослых": zhanr = Zhanr.Adult; break;
+                case "Для влюбленных": zhanr = Zhanr.Enamored; break;
+                case "Для отчаявшихся": zhanr = Zhanr.Desperate; break;
+            }
+            return zhanr;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TextBlock text = value as TextBlock;
+            
+            return text!.Text;
+        }
+    }
+}
---
+++ Lab12-13/Model/Book.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab12_13.Model
+{
+    public enum Zhanr
+    {
+        Children,
+        Adult,
+        Enamored,
+        Desperate
+    }
+    public class Book : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string? title;
+        public string? Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+        private Zhanr? bookZhanr;
+        public Zhanr? BookZhanr
+        {
+            get { return bookZhanr; }
+            set
+            {
+                bookZhanr = value;
+                OnPropertyChanged(nameof(BookZhanr));
+            }
+        }
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = String.Empty;
+                switch (columnName)
+                {
+                    case "Title":
+                        if (Title != null)
+                        {
+                            if (!Regex.IsMatch(Title!, @"[A-Za-z]+$"))
+                                error = "Имя не должно содержать вспомогательных символов";
+                        }
+                        else
+                        {
+                            error = "Имя не должно быть пустым";
+                        }
+                        break;
+                    case "BookZhanr":
+                        break;
+                }
+                return error;
+            }
+        }
+
+        public string Error => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
