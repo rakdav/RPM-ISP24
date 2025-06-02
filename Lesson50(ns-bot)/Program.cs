2f883b070e7c6966b77e5361ae186342a341a1f1
rakdav <rakdav@inbox.ru>
2025-06-02 12:20:14 +0200

    2.06-last

--- Lesson50(ns-bot)/Program.cs
+++ Lesson50(ns-bot)/Program.cs
@@ -78,16 +78,43 @@ async Task OnCommand(string command, string args, Message msg)
                 await bot.SendPhoto(msg.Chat, filestream, caption: "Read https://telegrambots.github.io/book/");
             }
             break;
+        case "/inline_buttons":
+            await bot.SendMessage(msg.Chat, "Inline buttons:", replyMarkup: new InlineKeyboardButton[][] {
+                ["1.1", "1.2", "1.3"],
+                [("WithCallbackData", "CallbackData"), ("WithUrl", "https://github.com/TelegramBots/Telegram.Bot")]
+            });
+            break;
+        case "/keyboard":
+            await bot.SendMessage(msg.Chat, "Keyboard buttons:", replyMarkup: new[] { "MENU", "INFO", "LANGUAGE" });
+            break;
+        case "/remove":
+            await bot.SendMessage(msg.Chat, "Removing keyboard", replyMarkup: new ReplyKeyboardRemove());
+            break;
+        case "/poll":
+            await bot.SendPoll(msg.Chat, "Question", ["Option 0", "Option 1", "Option 2"], isAnonymous: false, allowsMultipleAnswers: true);
+            break;
+        case "/reaction":
+            await bot.SetMessageReaction(msg.Chat, msg.Id, ["❤"], false);
+            break;
     }
 }
 async Task OnUpdate(Update update)
 {
-    
+    switch (update)
+    {
+        case { CallbackQuery: { } callbackQuery }: await OnCallbackQuery(callbackQuery); break;
+        case { PollAnswer: { } pollAnswer }: await OnPollAnswer(pollAnswer); break;
+        default: Console.WriteLine($"Received unhandled update {update.Type}"); break;
+    }
+    ;
 }
 async Task OnCallbackQuery(CallbackQuery callbackQuery)
 {
+    await bot.AnswerCallbackQuery(callbackQuery.Id, $"You selected {callbackQuery.Data}");
+    await bot.SendMessage(callbackQuery.Message!.Chat, $"Received callback from inline button {callbackQuery.Data}");
 }
 async Task OnPollAnswer(PollAnswer pollAnswer)
 {
-
+    if (pollAnswer.User != null)
+        await bot.SendMessage(pollAnswer.User.Id, $"You voted for option(s) id [{string.Join(',', pollAnswer.OptionIds)}]");
 }
